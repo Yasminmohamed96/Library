b5533b87711e9a4c4a08a6856c41a62eb3ac36c2
Yasminmohamed96 <myasmin91@yahoo.com>
2021-10-31 16:33:57 +0200

    final version

--- src/app.js
+++ src/app.js
@@ -4,22 +4,28 @@ require("../db/connection");
 
 //call required packages
 const express = require("express");
-// const cors = require("cors")
+const cors = require("cors");
 //create express intance
 const app = express();
-// app.use(cors())
+const path = require("path");
+
+app.use(cors());
 //use json and urlencoded middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.get("/findAsset/:name", (req, res) => {
+//   let name = path.join(__dirname, "../public/", req.params.name);
+//   res.sendFile(name);
+// });
 //routes files
 const userRoutes = require("../routes/user.routes");
 const bookRoutes = require("../routes/book.routes");
 const adminRoutes = require("../routes/admin.routes");
-const autherRoutes = require("../routes/auther.routes");
+const anyRoutes = require("../routes/any.routes");
 
 app.use("/user", userRoutes); // 45 r   /user/x
 app.use("/book", bookRoutes); // 35 r => app.get('/x')  /post/x
 app.use("/admin", adminRoutes);
-app.use("/auther", autherRoutes);
+//app.use("/", anyRoutes);
 module.exports = app;
